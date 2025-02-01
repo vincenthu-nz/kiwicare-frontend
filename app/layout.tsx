403150af381c9c4c0ad366592ab37f49654d4a8b
Vincent Hu <vincent.hu@outlook.co.nz>
2025-02-01 18:04:04 +1300

    First Init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import "@/app/ui/global.css";
+import React from "react";
+
+export default function RootLayout(
+    {
+        children,
+    }: {
+        children: React.ReactNode;
+    }) {
+    return (
+        <html lang="en">
+        <body>{children}</body>
+        </html>
+    );
 }
